58ee96864e11aa064073d9f4845cf47d82909ca4
Kris Jones <kristop.jones@gmail.com>
2021-04-11 20:54:36 -0600

    loop to shuffle

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.querySelector('.grid')
-  const flagsLeft = document.querySelector('#flags-left')
-  const result = document.querySelector('#result')
-  let width = 10
-  let bombAmount = 10
-  let flags = 0
-  let squares = []
-  let isGameOver = false
+    const grid = document.querySelector('.grid')
+    const flagsLeft = document.querySelector('#flags-left')
+    const result = document.querySelector('#result')
+    let width = 10
+    let bombAmount = 10
+    let flags = 0
+    let squares = []
+    let isGameOver = false
 
-  //create Board
-  function createBoard() {
-      // @ts-ignore
-      flagsLeft.innerHTML = bombAmount
+    //create Board
+    function createBoard() {
+        // @ts-ignore
+        flagsLeft.innerHTML = bombAmount
       
-    //get shuffled game array with random bombs
-    const bombsArray = Array(bombAmount).fill('bomb')
-    const emptyArray = Array(width*width - bombAmount).fill('valid')
-    const gameArray = emptyArray.concat(bombsArray)
-    const shuffledArray = gameArray.sort(() => Math.random() -0.5)
+        //get shuffled game array with random bombs
+        const bombsArray = Array(bombAmount).fill('bomb')
+        const emptyArray = Array(width * width - bombAmount).fill('valid')
+        const gameArray = emptyArray.concat(bombsArray)
+        const shuffledArray = gameArray.sort(() => Math.random() - 0.5)
+      
+        for (let i = 0; i < width * width; i++) {
+            const square = document.createElement('div')
+            // @ts-ignore
+            square.setAttribute('id', i)
+            square.classList.add(shuffledArray[i])
+            grid.appendChild(square)
+            squares.push(square)
+
+
+
+
+
+        }
+    }
 }
