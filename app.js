74b0be9810a11aafb54cb98127c1222c3c285c0a
Kris Jones <kristop.jones@gmail.com>
2021-04-13 18:09:35 -0600

    gameover/checkforwin

--- app.js
+++ app.js
@@ -160,3 +160,17 @@ document.addEventListener('DOMContentLoaded', () => {
         }
             
     
+  //game over
+  function gameOver(square) {
+    result.innerHTML = 'BOOM! Game Over!'
+    isGameOver = true
+
+    //show ALL the bombs
+    squares.forEach(square => {
+      if (square.classList.contains('bomb')) {
+        square.innerHTML = '💣'
+        square.classList.remove('bomb')
+        square.classList.add('checked')
+      }
+    })
+  }
